9a8c3826999da2c259f7dab5914515cef259824f
Guillaume Gimenez <ploki@blackmilk.fr>
2023-06-22 23:44:55 +0200

    temporal domain fixes

--- src/simulation_impulse.c
+++ src/simulation_impulse.c
@@ -98,33 +98,33 @@ void simulation_impulse(simulation_context_t *sc,
   gsl_fft_complex_wavetable * wavetable = gsl_fft_complex_wavetable_alloc (n_samples);
   gsl_fft_complex_workspace * workspace = gsl_fft_complex_workspace_alloc (n_samples);
 
-  yana_complex_t square_wave[n_samples];
+  yana_complex_t the_wave[n_samples];
   yana_complex_t dc;
 
   if (fe > 0 )
-    dc = build_square_wave(square_wave, n_samples, fe/4.);
+    dc = build_square_wave(the_wave, n_samples, fe/4.);
   else if (fe == 0)
-    dc = build_impulse(square_wave, n_samples);
+    dc = build_impulse(the_wave, n_samples);
   else if (fe == -1)
-    dc = build_step(square_wave, n_samples);
+    dc = build_step(the_wave, n_samples);
   else if (fe < -1)
-    dc = build_cis(square_wave, n_samples, -fe/4.);
+    dc = build_cis(the_wave, n_samples, -fe/4.);
   else dc = 0;
 
   for (int i = 0; i < n_samples/2; ++i)
     {
-      REAL(data, i) = creal(square_wave[i * 2]);
-      IMAG(data, i) = cimag(square_wave[i * 2]);
-      REAL(data, n_samples - i - 1) = creal(square_wave[i * 2 + 1]);
-      IMAG(data, n_samples - i - 1) = cimag(square_wave[i * 2 + 1]);
+      REAL(data, i) = creal(the_wave[i * 2]);
+      IMAG(data, i) = cimag(the_wave[i * 2]);
+      REAL(data, n_samples - i - 1) = creal(the_wave[i * 2 + 1]);
+      IMAG(data, n_samples - i - 1) = cimag(the_wave[i * 2 + 1]);
     }
   if (1)
     gsl_fft_complex_forward(data, 1, n_samples,
                             wavetable, workspace);
   for(int i = 0; i < n_samples / 2; ++i) {
-    square_wave[i * 2] = (    REAL(data, i)
+    the_wave[i * 2] = (    REAL(data, i)
                               + I * IMAG(data, i));
-    square_wave[i * 2 + 1] = (REAL(data, n_samples - i - 1)
+    the_wave[i * 2 + 1] = (REAL(data, n_samples - i - 1)
                               + I * IMAG(data, n_samples - i - 1));
   }
 
@@ -133,8 +133,8 @@ void simulation_impulse(simulation_context_t *sc,
     for (int i = 0; i < n_samples/2; ++i)
       {
         yana_complex_t
-          f_even = square_wave[i * 2],
-          f_odd  = square_wave[i * 2 + 1],
+          f_even = the_wave[i * 2],
+          f_odd  = the_wave[i * 2 + 1],
           even = f_even * (i == 0 ? dc : result[i * 2 - 1]),
           odd  = f_odd * result[i * 2];
         REAL(data, i) = creal(even);
